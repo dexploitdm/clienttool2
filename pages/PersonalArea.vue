c5742402a2a0bc67fa3b835110a1f54c5d86aa84
dexploitdm <knutdmit@yandex.ru>
2020-07-18 12:39:06 +0500

    button

--- pages/PersonalArea.vue
+++ pages/PersonalArea.vue
@@ -133,7 +133,10 @@
 
                     </div>
                     <aside>
-                        <div class="filters-content-box">
+                        <div class="button-filter--visible" @click="filterVisible = !filterVisible">
+                            <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="filter" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="svg-inline--fa fa-filter fa-w-16 fa-3x"><path fill="currentColor" d="M487.976 0H24.028C2.71 0-8.047 25.866 7.058 40.971L192 225.941V432c0 7.831 3.821 15.17 10.237 19.662l80 55.98C298.02 518.69 320 507.493 320 487.98V225.941l184.947-184.97C520.021 25.896 509.338 0 487.976 0z" class=""></path></svg>
+                        </div>
+                        <div :class="{ 'is-active': filterVisible }" class="filters-content-box">
                             <form action="">
                                 <div class="filters-box"  :class="{ 'is-active': inputEl }">
                                     <h4 @click="inputEl = !inputEl" :class="{ 'is-active': inputEl }">Коллаборации</h4>
@@ -329,6 +332,7 @@ export default {
             inputEl3:false,
             inputEl4:false,
             mobile_list: null,
+            filterVisible:null,
             colors: ['#99A9BF', '#F7BA2A', '#FF9900'],
             descriptionElement: [
                 {
